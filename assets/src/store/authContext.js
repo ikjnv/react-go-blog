6a17e6488fdfd16da1ceb3044b684faa861c5d05
ikjnv <abdurauf.isokjonov@gmail.com>
2021-12-08 11:10:01 +0900

    working client-side
    * added simple home page component
    
    * added Authentication context to share state across components

---
+++ assets/src/store/authContext.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const authContext = React.createContext({
+  username: null,
+  token: null,
+  logedIn: false,
+  login: (token) => {},
+  logout: () => {}
+});
+
+export const AuthContextProvider = (props) => {
+  const tokenKey = '';
+  const usrName = '';
+  const [username, setUsername] = useState(localStorage.getItem(usrName));
+  const [token, setToken] = useState(localStorage.getItem(tokenKey));
+  const loggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem(tokenKey, token);
+  };
+
+  const userNameHandler = (username) => {
+    setUsername(username);
+    localStorage.setItem(usrName, username);
+  }
+
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem(tokenKey);
+    localStorage.removeItem(usrName);
+  };
+
+  const contextValue = {
+    username: username,
+    token: token,
+    loggedIn: loggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+    setUsername: userNameHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
