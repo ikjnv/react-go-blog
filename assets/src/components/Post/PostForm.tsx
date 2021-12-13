6f27612617859ff923e0ea79f5a554bdbcd5a768
ikjnv <abdurauf.isokjonov@gmail.com>
2021-12-13 10:20:35 +0900

    typed post form
    * create post working properly

--- assets/src/components/Post/PostForm.tsx
+++ assets/src/components/Post/PostForm.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useContext, useCallback } from 'react';
 import AuthContext from '../../store/authContext';
+import Errors from '../Errors/Errors';
 import {PostProps} from './interfaces';
 import { FormBlock, Form } from "./styled"
 
@@ -20,45 +21,60 @@ export default function PostForm(post: PostProps) {
 		populateFields();
 	}, [populateFields]);
 
+	const titleChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+		setTitle(e.target.value);
+	};
+
+	const contentChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+		setContent(e.target.value);
+	};
+
 	async function submitHandler(e: React.FormEvent) {
 		e.preventDefault();
 		setErrors({});
 
 		try {
+			const res = await fetch('/api/v1/posts',
+				{
+					method: 'POST',
+					body: JSON.stringify({
+						Title: title,
+						Content: content,
+					}),
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${context.token}`
+					}
+				}
+			);
 
-		const res = await fetch('/api/v1/posts',
-			{
-				method: 'POST',
-				body: JSON.stringify({
-					Title: title,
-					Content: content,
-				}),
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${context.token}`
+			const data = await res.json();
+			if(!res.ok) {
+				let errorTxt = 'Post creation process failed';
+				if(!data.hasOwnProperty('error')) {
+					throw new Error(errorTxt);
+				}
+				if(typeof data['error'] === 'string') {
+					setErrors({'unknown': data['error']});
+				} else {
+					setErrors(data['error']);
+				}
+			} else {
+				setTitle('');
+				setContent('');
+				if(post.addPostHandler) {
+					post.addPostHandler(data.data);
+				}
+				if(post.editPostHandler) {
+					post.editPostHandler(data.data);
 				}
 			}
-		);
-
-		const data = await res.json();
-		if(!res.ok) {
-			let errorTxt = loggingIn ? 'Login failed' : 'Sign up failed';
-			
-			if(!data.hasOwnProperty('error')) {
-				throw new Error(errorTxt);
-			}
-
-			if(typeof data.error === 'string') {
-				setErrors({'unknown': data.error});
-			} else {
-				setErrors(data.error);
-			}
-		}
-
-		} catch (err) {
-			setErrors({ "error": err.message });
+		} catch (err: any) {
+			setErrors({"error": err.message});
 		}
 	}
+
+	const errorContent = Object.keys(errors).length === 0 ? null : Errors(errors);
 
 	return (
 		<FormBlock>
@@ -69,6 +85,7 @@ export default function PostForm(post: PostProps) {
 						type="text"
 						placeholder="Post title"
 						value={title}
+						onChange={titleChangeHandler}
 					/>
 				</div>
 				<div>
@@ -76,10 +93,12 @@ export default function PostForm(post: PostProps) {
 						type="text"
 						placeholder="Content"
 						value={content}
+						onChange={contentChangeHandler}
 					/>
 				</div>
 				<button>Create</button>
 			</Form>
+			{errorContent}
 		</FormBlock> 
 	)
 };
